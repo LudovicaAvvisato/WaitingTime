c0910acd9cf840dde5b21d02c28c5451a75e67d3
studente <test@gmail.com>
2026-01-19 13:03:43 +0100

    Prima Prova WaitingTime ufficiale

---
+++ src/main/java/org/example/BancaMiele.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class BancaMiele {
+    public static void main(String[] args) {
+
+        BufferedReader Lettore= new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Lettore);
+    }
+}
